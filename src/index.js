0a890c74d2aab3b86f1387a3cbf836e9c226a7ad
Sonjit Saha <sonjitsaha78@gmail.com>
2022-09-04 09:53:12 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCcfCjnJafCERh7ezwIV6SmPI0kouYAI1Y",
-  authDomain: "chat-app-4ef68.firebaseapp.com",
-  databaseURL: "https://chat-app-4ef68-default-rtdb.firebaseio.com",
-  projectId: "chat-app-4ef68",
-  storageBucket: "chat-app-4ef68.appspot.com",
-  messagingSenderId: "402561867056",
-  appId: "1:402561867056:web:7ac9883ab0a7641bf83cf5"
+  apiKey: "AIzaSyBTfC2HzSkWP7ERL6TIkf6MssO4ihuD0vE",
+  authDomain: "chating-app-f02d0.firebaseapp.com",
+  databaseURL: "https://chating-app-f02d0-default-rtdb.firebaseio.com",
+  projectId: "chating-app-f02d0",
+  storageBucket: "chating-app-f02d0.appspot.com",
+  messagingSenderId: "1055272698447",
+  appId: "1:1055272698447:web:56f5b733e627ebc433a03a"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
